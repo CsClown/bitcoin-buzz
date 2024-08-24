73abc72cfa2af6939a904db375c567f7fa7a2f38
Stefan Ruppe <ruppe@protonmail.com>
2024-08-24 11:17:27 +0000

    basic models implementation for post, reply, topic

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Post, Topic, Reply
 
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Topic)
+admin.site.register(Reply)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils.text import slugify #chatgpt suggestion of auto-slugs
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Topic(models.Model):
+    title = models.CharField(max_length=200)
+
+    def get_topic_posts_count(self):
+        return self.topic_posts.count()
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="forum_posts")
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True, related_name="topic_posts")
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+    #chat gpt suggestion of auto-slugs
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
+
+class Reply(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_replies")
+    related_post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post_replies")
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
