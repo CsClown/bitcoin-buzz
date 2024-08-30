6cc009568fa0582fc82fbda45bda707533d3bf2e
Stefan Ruppe <ruppe@protonmail.com>
2024-08-30 08:08:19 +0000

    final commit

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,22 @@
+from allauth.socialaccount.models import SocialAccount, SocialApp, SocialToken
 from django.contrib import admin
+from django.contrib.sites.models import Site
 from django.contrib.auth.models import Group
 from .models import Post, Topic, Reply, UserProfile
 
 
 # Register your models here.
 admin.site.unregister(Group)
+
+
+# Unregister the social account models
+admin.site.unregister(SocialAccount)
+admin.site.unregister(SocialApp)
+admin.site.unregister(SocialToken)
+
+
+# Unregister the Site model from the sites framework
+admin.site.unregister(Site)
 
 
 class PostAdmin(admin.ModelAdmin):
